012f30c160d4e2f340805b6a3ec4768f15d0720b
2022KirillMaximov <102306817+2022KirillMaximov@users.noreply.github.com>
2023-07-12 22:37:05 +0200

    task 2 error

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const CURRRENT_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe, {
+    loop: true,
+    fullscreen: true,
+    quality: '1080p',
+});
+
+const getCurrentTime = function (currentTime) {
+    const seconds = currentTime.seconds;
+    localStorage.setItem(CURRRENT_TIME_KEY, JSON.stringify(seconds))
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
