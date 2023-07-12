32bf00f673c2232fe8fccc8af2f04ff390f96d5d
2022KirillMaximov <102306817+2022KirillMaximov@users.noreply.github.com>
2023-07-12 20:36:59 +0200

    add gallery js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,30 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+
+function createMarkuo(arr) {
+    return arr.map(({  preview, original, description }) =>
+        
+        `<li class="gallery__item">
+  <a class="gallery__link"  href="${original}">
+    <img 
+    class="gallery__image"
+    src=  "${preview}" 
+    alt="${description}">
+  </a>
+</li>`
+
+    ).join('')
+}
+gallery.insertAdjacentHTML('afterbegin', createMarkuo(galleryItems));
+
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  
+  captionsData: `alt`, captionDelay: 250,
+})
+
