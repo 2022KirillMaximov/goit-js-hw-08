656cdd3600dbc6fb0e4a5990fd808311376bd99c
2022KirillMaximov <102306817+2022KirillMaximov@users.noreply.github.com>
2023-07-13 19:47:39 +0200

    2 task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -20,3 +20,19 @@ const getCurrentTime = function (currentTime) {
 
 player.on('timeupdate', throttle(getCurrentTime, 1000));
 
+const savedTime = localStorage.getItem(CURRRENT_TIME_KEY )
+
+player.setCurrentTime(savedTime)
+    .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
