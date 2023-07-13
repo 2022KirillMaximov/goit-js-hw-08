22d13a772e0c95144b8973466fae309fb32376fd
2022KirillMaximov <102306817+2022KirillMaximov@users.noreply.github.com>
2023-07-13 21:14:52 +0200

    task 3 mistake

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
 
+import throttle from 'lodash.throttle'; 
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form message'),
+
+}
+
+
+
+refs.form.addEventListener("submit", onFormSubmit);
+refs.textarea.addEventListener("input", onTextareaInput);
+
+function onFormSubmit(evt) { 
+    evt.preventDefault();
+    evt.currentTarget.reset();
+}
+
+
+function onTextareaInput(evt) {
+    const message = evt.currentTarget.value;
+
+    localStorage.setItem("feedback-form-state", massage);
+}
